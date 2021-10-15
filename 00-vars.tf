cad8215e450b1746c3d31efeaa5b51d848ed5810
Daniel Johansson <klusbert@gmail.com>
2021-10-15 10:24:29 +0200

    creates an image with latest updates, and nfs-common package

---
+++ 00-vars.tf
@@ -0,0 +1,37 @@
+variable "imagename" {
+  type    = string
+  default = "baked_image_1.0"
+}
+
+
+
+variable "availability_zone" {
+  type    = string
+  default = "Education"
+}
+variable "image_id" {
+  type    = string
+  default = "ca4bec1a-ac25-434f-b14c-ad8078ccf39f"
+}
+variable "flavor_id" {
+  type    = string
+  default = "c1-r05-d5"
+}
+variable "router_name" {
+  type    = string
+  default = "packer_router"
+}
+
+variable "network_name" {
+  type    = string
+  default = "packer_network"
+}
+variable "subnet_name" {
+  type    = string
+  default = "packer_subnet"
+}
+
+variable "external_network_id" {
+  type    = string
+  default = "fd401e50-9484-4883-9672-a2814089528c"
+}
